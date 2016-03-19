19f22c127f467d14798bfab4f600f1354d5b32ef
chang-ning <spiderpower02@gmail.com>
2016-03-19 14:56:00 +0800

    Add ssl forward

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ This is a simple cheatsheet webapp.
 import os
 
 from flask import Flask, send_from_directory
+from flask_sslify import SSLify
 
 DIR = os.path.dirname(os.path.realpath(__file__))
 ROOT = os.path.join(DIR, 'docs', '_build', 'html')
 
 app = Flask(__name__)
+if 'DYNO' in os.environ:
+        sslify = SSLify(app)
 
 @app.route('/<path:path>')
 def static_proxy(path):
@@ -21,4 +24,4 @@ def index_redirection():
     return send_from_directory(ROOT, 'index.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
