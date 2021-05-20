2b355cd0f962dbc9410c5155db838628cb42a8ca
chang-ning <spiderpower02@gmail.com>
2021-05-20 08:16:07 +0800

    add unittest
    Signed-off-by: chang-ning <spiderpower02@gmail.com>

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
-"""
-This is a simple cheatsheet webapp.
-"""
+"""This is a simple cheatsheet webapp."""
+
 import os
 
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, abort, send_from_directory, render_template
 from flask_sslify import SSLify
+from flask_seasurf import SeaSurf
 from flask_talisman import Talisman
 
 DIR = os.path.dirname(os.path.realpath(__file__))
 ROOT = os.path.join(DIR, "docs", "_build", "html")
 
-app = Flask(__name__, template_folder=ROOT)
-if "DYNO" in os.environ:
-    sslify = SSLify(app, permanent=True)
+
+def find_key(token):
+    """Find the key from the environment variable."""
+    if token == os.environ.get("ACME_TOKEN"):
+        return os.environ.get("ACME_KEY")
+    for k, v in os.environ.items():
+        if v == token and k.startswith("ACME_TOKEN_"):
+            n = k.replace("ACME_TOKEN_", "")
+            return os.environ.get("ACME_KEY_{}".format(n))
+
 
 csp = {
     "default-src": "'none'",
@@ -22,6 +29,8 @@ csp = {
         "*.cloudflare.com",
         "*.googletagmanager.com",
         "*.google-analytics.com",
+        "*.carbonads.com",
+        "*.carbonads.net",
         "'unsafe-inline'",
         "'unsafe-eval'",
     ],
@@ -34,12 +43,24 @@ csp = {
 }
 
 feature_policy = {"geolocation": "'none'"}
+
+app = Flask(__name__, template_folder=ROOT)
+app.config["SECRET_KEY"] = os.urandom(16)
+app.config["SESSION_COOKIE_NAME"] = "__Secure-session"
+app.config["SESSION_COOKIE_SAMESITE"] = "Strict"
+app.config["CSRF_COOKIE_NAME"] = "__Secure-csrf-token"
+app.config["CSRF_COOKIE_HTTPONLY"] = True
+app.config["CSRF_COOKIE_SECURE"] = True
+csrf = SeaSurf(app)
 talisman = Talisman(
     app,
     force_https=False,
     content_security_policy=csp,
     feature_policy=feature_policy,
 )
+
+if "DYNO" in os.environ:
+    sslify = SSLify(app, permanent=True, skips=[".well-known"])
 
 
 @app.errorhandler(404)
@@ -50,14 +71,24 @@ def page_not_found(e):
 
 @app.route("/<path:path>")
 def static_proxy(path):
-    """Static files proxy"""
+    """Find static files."""
     return send_from_directory(ROOT, path)
 
 
 @app.route("/")
 def index_redirection():
-    """Redirecting index file"""
+    """Redirecting index file."""
     return send_from_directory(ROOT, "index.html")
+
+
+@csrf.exempt
+@app.route("/.well-known/acme-challenge/<token>")
+def acme(token):
+    """Find the acme-key from environment variable."""
+    key = find_key(token)
+    if key is None:
+        abort(404)
+    return key
 
 
 if __name__ == "__main__":
